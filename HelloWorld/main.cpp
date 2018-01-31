c32ded41b1c803f8e8c0ea0130bb0b2881af606a
George Strauch <35750484+gstrauch2@users.noreply.github.com>
2018-01-30 19:48:22 -0600

    Add files via upload

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * George Strauch
+ * ITSE 1307
+ * Hello World
+ * */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
